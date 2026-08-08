ab2c4cf3129b4ab20d8a565492205acdc3f4ddd8
Aravind Pagidimari <106377794+arvind88765@users.noreply.github.com>
2026-08-08 23:33:46 +0530

    Update GenZGift.jsx

--- app/components/GenZGift.jsx
+++ app/components/GenZGift.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import confetti from 'canvas-confetti';
 
 export default function GenZGift() {
